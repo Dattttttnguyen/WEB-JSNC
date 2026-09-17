6b1ee2ad370b36e31bafec372ce45d3ab10429fa
dat <nguyendat129273@gmail.com>
2026-09-17 15:42:40 +0700

    datt

---
+++ jsnc_fa26-main/js.js
@@ -0,0 +1,9 @@
+let name = "Nguyễn Tiến Đạt";
+let age = 23;
+let address = "Hà Nội";
+let isStudent = true;
+
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
